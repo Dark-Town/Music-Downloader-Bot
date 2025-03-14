3d15fbc3068e0cd40ed6a468f0601e68bc7cb736
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-03-14 11:46:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("7919283586:AAFIq7A0LuVG5nCdxBnWheNHf_cglXh8GQU", "")
     API_ID = int(os.environ.get("APP_ID", ))
     API_HASH = os.environ.get("API_HASH", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
+    LOG_CHANNEL = int(os.environ.get("t.me/tcronebhackx", -100))
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
     SESSION_NAME = os.environ.get("SESSION_NAME", "")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", ))
+    BOT_OWNER = int(os.environ.get("TcronebNet", ))
